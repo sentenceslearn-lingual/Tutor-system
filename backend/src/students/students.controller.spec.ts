15aa28e61ce46de2154e6c8df22787b27dcbe2a4
thitikornttkwork-cmyk <thitikorn.ttk.work@gmail.com>
2026-08-01 02:17:17 +0700

    Fix Jest TypeScript configuration

--- backend/src/students/students.controller.spec.ts
+++ backend/src/students/students.controller.spec.ts
@@ -1,18 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { StudentsController } from './students.controller';
+import { StudentsService } from './students.service';
+
 
 describe('StudentsController', () => {
+
   let controller: StudentsController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [StudentsController],
-    }).compile();
 
-    controller = module.get<StudentsController>(StudentsController);
+  beforeEach(async () => {
+
+    const module: TestingModule =
+      await Test.createTestingModule({
+
+        controllers: [
+          StudentsController,
+        ],
+
+        providers: [
+          {
+            provide: StudentsService,
+
+            useValue: {
+
+              registerStudent: jest.fn(),
+
+              getStudents: jest.fn(),
+
+              updateStudent: jest.fn(),
+
+              deleteStudent: jest.fn(),
+
+            },
+
+          },
+
+        ],
+
+      }).compile();
+
+
+    controller =
+      module.get<StudentsController>(
+        StudentsController,
+      );
+
   });
+
+
 
   it('should be defined', () => {
+
     expect(controller).toBeDefined();
+
   });
+
 });
