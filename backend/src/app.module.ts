e073ca67a95cd2e885f6a357d4cc00e001a60bdd
thitikornttkwork-cmyk <thitikorn.ttk.work@gmail.com>
2026-07-31 22:02:30 +0700

    Phase 3 - Setup Prisma database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [StudentsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    StudentsModule,
+    PrismaModule,
+  ],
+  controllers: [
+    AppController,
+  ],
+  providers: [
+    AppService,
+  ],
 })
 export class AppModule {}
